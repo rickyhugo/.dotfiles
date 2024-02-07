a3582c6c48b9f02bc0de14b6fac29da128523507
rickyhugo <hugo.englund7@gmail.com>
2024-02-07 10:00:55 +0100

    feat: lazygit

--- .my-shit/installer.sh
+++ .my-shit/installer.sh
@@ -42,6 +42,12 @@ sudo apt install i3lock xautolock
 sudo apt install git-all -y
 git config --global core.excludesfile "$MY_GITIGNORE"
 
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 # xclip
 sudo apt install xclip -y
 
