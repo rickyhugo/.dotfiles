d37efe0d0604735e76a261e646fd3376162609f3
rickyhugo <hugo.englund7@gmail.com>
2024-02-12 16:51:59 +0100

    feat: lint

--- .config/nvim/lua/huen/plugins/nvim-lint.lua
+++ .config/nvim/lua/huen/plugins/nvim-lint.lua
@@ -18,18 +18,17 @@ return {
 			markdown = { "markdownlint" },
 			yaml = { "yamllint" },
 			lua = { "luacheck" },
+			python = { "mypy" },
 		}
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
 				lint.try_lint()
 			end,
 		})
 
 		local lint_progress = function()
-			local linters = require("lint").get_running()
+			local linters = lint.get_running()
 			if #linters == 0 then
 				print("❌ No linters running...")
 			else
