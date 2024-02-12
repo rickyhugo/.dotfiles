de77da9d9a8a73afa04a876216ee36a108654766
rickyhugo <hugo.englund7@gmail.com>
2024-02-12 12:13:01 +0100

    feat: gh

--- .my-shit/installer.sh
+++ .my-shit/installer.sh
@@ -193,6 +193,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
     && sudo apt update \
     && sudo apt install gh -y
+gh config set editor nvim
 
 # chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
