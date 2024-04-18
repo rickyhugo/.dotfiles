f44195cfa7c1daec1fbd7c3de293ee1f0cd68644
rickyhugo <hugo.englund7@gmail.com>
2024-04-18 19:20:50 +0200

    feat: remove eslint, use lsp instead

--- .config/nvim/lua/huen/plugins/nvim-lint.lua
+++ .config/nvim/lua/huen/plugins/nvim-lint.lua
@@ -5,11 +5,6 @@ return {
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
 			sql = { "sqlfluff" },
 			zsh = { "zsh" },
 			sh = { "shellcheck" },
